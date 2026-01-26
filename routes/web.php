4800070fd2547fd93d1e28cd93c1dd6ac0a1db9f
山本晃司 <koji.19@icloud.com>
2026-01-26 22:55:37 +0900

    管理者側の店舗管理機能

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,7 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin
 
 Route::get('users', [Admin\UserController::class, 'index'])->name('users.index');
 Route::get('users/{user}', [Admin\UserController::class, 'show'])->name('users.show');
+
+Route::resource('restaurants', Admin\RestaurantController::class);
+
 });
